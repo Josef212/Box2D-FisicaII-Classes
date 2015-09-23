dc586df50b4f5fc138cb1e7ad745988d20b98959
Josep Casanovas <jcasanovasv@hotmail.com>
2015-09-23 13:01:32 +0200

    Classe1

---
+++ Globals.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+
+#define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__);
+
+void log(const char file[], int line, const char* format, ...);
+
+#define CAP(n) ((n <= 0.0f) ? n=0.0f : (n >= 1.0f) ? n=1.0f : n=n)
+
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
+
+typedef unsigned int uint;
+
+enum update_status
+{
+	UPDATE_CONTINUE = 1,
+	UPDATE_STOP,
+	UPDATE_ERROR
+};
+
+// Configuration -----------
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 758
+#define SCREEN_SIZE 1
+#define WIN_FULLSCREEN false
+#define WIN_RESIZABLE false
+#define WIN_BORDERLESS false
+#define WIN_FULLSCREEN_DESKTOP false
+#define VSYNC true
+#define TITLE "2D Physics Playground"
+#define GRAVITY_X 0.0f
+#define GRAVITY_Y -9.81f
+#define VELOCITY_ITERATIONS 6//8
+#define POSITION_ITERATION 2//3
+#define TIME_STEP 1.0f/60.0f
+
+#define PIXTOMET(pix) (pix*(1.0f/150.0f))
+#define METTOPIX(met) (met*150)
